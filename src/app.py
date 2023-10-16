c99b458b55d4fe4759d9681d2f7a6eba9883319a
AlexanderLarsen343 <alexander.m.larsen@wsu.edu>
2023-10-15 18:46:20 -0700

    Added WSU image to homepage

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="View/templates")
+app = Flask(__name__, template_folder="View/templates", static_folder="View/static")
 
 @app.route("/")
 def index():
