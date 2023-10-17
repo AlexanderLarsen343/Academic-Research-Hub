adf7399f2fca71e42f96e4cfbabe15243c37f485
AlexanderLarsen343 <alexander.m.larsen@wsu.edu>
2023-10-16 17:07:37 -0700

    Began `/login` route

--- src/Controller/auth_routes.py
+++ src/Controller/auth_routes.py
@@ -1,7 +1,18 @@
 from flask import Blueprint
+from flask_login import login_user, current_user, logout_user, login_required
+
+from src.Controller.auth_forms import LoginForm
+from src.Model.models import User, Student, Professor
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login")
 def login():
-    pass
+    if current_user.is_authenticated:
+        if current_user.user_type == "Student":
+            return redirect(url_for('routes.student_homepage')) # TODO: Change to actual student homepage
+        elif current_user.user_type == "Professor":
+            return redirect(url_for('routes.professor_homepage')) # TODO: Change to actual prof. homepage
+    lform = LoginForm()
+    if lform.validate_on_submit():
+        user = User.query.filter_by(username = lform.usernmae)
