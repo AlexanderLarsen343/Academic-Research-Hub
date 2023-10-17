a3df0eeb95fbb8d029b97ce7115ec6e105e2f371
Gabriel Hartz <gabriel.hartz@gmail.com>
2023-10-16 18:30:28 -0700

    Language and Interest tables added to the models.py, which have a relationship established with the student model

--- src/Model/models.py
+++ src/Model/models.py
@@ -3,6 +3,14 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+studentInterests = db.Table('studentInterests',
+                db.Column('student_id', db.Integer, db.ForeignKey('student.id')),
+                db.Column('interest_id', db.Integer, db.ForeignKey('interest.id')))
+
+studentLanguages = db.Table('studentLanguages',
+                db.Column('student_id', db.Integer, db.ForeignKey('student.id')),
+                db.Column('language_id', db.Integer, db.ForeignKey('language.id')))
+
 #Parent class for both Student and Professor
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -41,11 +49,35 @@ class Student(User, db.Model):
     languages = db.Column(db.String(50))
     interests = db.Column(db.String(150))
     experience = db.Column(db.String(500))
+    
+    interests = db.relationship('Interest',
+                                secondary=studentInterests,
+                                primaryjoin=(studentInterests.c.student_id == id),
+                                backref=db.backref('studentInrests', lazy='dynamic'),
+                                lazy='dynamic')
+    languages = db.relationship('Language',
+                                secondary=studentLanguages,
+                                primaryjoin=(studentLanguages.c.student_id == id),
+                                backref=db.backref('studentLanguages', lazy='dynamic'),
+                                lazy='dynamic')
+    def get_interests(self):
+        return self.interests
+    
+    def get_languages(self):
+        return self.languages
 
     #Designates the identity of user_type
     __mapper_args__ = {
         'polymorphic_identity': 'Student'
     }
+    
+class Interest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(12))
+    
+class Language(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(12))
     
 class Professor(User, db.Model):
     __tablename__ = 'professor'
