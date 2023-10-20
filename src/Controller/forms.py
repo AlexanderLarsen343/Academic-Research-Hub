daf4410e79df7e443ee42afa3d7f1bd5da3fa0c1
Erick Pairault <123699675+Erick-Pairault@users.noreply.github.com>
2023-10-19 19:26:46 -0700

    added forms, routes, a createPosition page.

---
+++ src/Controller/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField, TextAreaField, BooleanField
+from wtforms.validators import  ValidationError, DataRequired, EqualTo, Length, Email
+from src.Model.models import User, Language, Interest
+from wtforms_sqlalchemy.fields import QuerySelectMultipleField
+from wtforms.widgets import CheckboxInput, ListWidget
+from flask_login import current_user
+
+class PositionForm(FlaskForm):
+	title = StringField('Title', validators=[DataRequired(), Length(max=32)]) # not sure if max is right here
+	description = TextAreaField('Description', validators=[DataRequired(),Length(max=1500)])
+	start_date = SelectField('Start Date',)
+	end_date = SelectField('End Date',)
+	workload = StringField('Workload', validators=[DataRequired()])
+	research_field =  StringField('Research Field', validators=[DataRequired(), Length(max=32)])
+	#languages = QuerySelectMultipleField('Tag', query_factory=get_tags, get_label=get_tag_label, widget=ListWidget(prefix_label=False), option_widget=CheckboxInput())
+	#requirements = QuerySelectMultipleField('Tag', query_factory=get_tags, get_label=get_tag_label, widget=ListWidget(prefix_label=False), option_widget=CheckboxInput())
+	submit = SubmitField('Post')
--- src/Controller/routes.py
+++ src/Controller/routes.py
@@ -1,7 +1,30 @@
-from flask import Blueprint, render_template
+from flask import Blueprint
+from flask import render_template, flash, redirect, url_for, request
+from config import Config
+from flask_login import current_user, login_required
+
+from src import db
+from src.Model.models import Position
+from src.Controller.forms import PositionForm
 
 routes = Blueprint("routes", __name__)
 
 @routes.route("/")
 def index():
     return render_template("index.html")
+
+@routes.route('/postposition/', methods = ['GET', 'POST'])
+@login_required
+def createPosition():
+    pform = PositionForm()
+    if pform.validate_on_submit():
+        newPostition = Position(title=pform.title.data, body=pform.body.data, happiness_level = pform.happiness_level.data, user_id = current_user.id)
+        for t in pform.languages.data:
+            newPostition.languages.append(t)
+        for t in pform.requirements.data:
+            newPostition.requirements.append(t)
+        db.session.add(newPostition)
+        db.session.commit()
+        flash('New Post: "' + newPostition.title + '" has been uploaded.')
+        return redirect(url_for('routes.index'))
+    return render_template('createPosition.html', form = pform)
